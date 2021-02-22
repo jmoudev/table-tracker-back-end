61ee0d3cb160e753810eb5dcc27cead282ea8997
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-22 16:24:37 +0000

    creates basic routes

---
+++ routers/api.router.js
@@ -0,0 +1,13 @@
+const foodItemsRouter = require('./food-items.router');
+const ordersRouter = require('./orders.router');
+const tablesRouter = require('./tables.router');
+const usersRouter = require('./users.router');
+
+const apiRouter = require('express').Router();
+
+apiRouter.use('/foodItems', foodItemsRouter);
+apiRouter.use('/orders', ordersRouter);
+apiRouter.use('/tables', tablesRouter);
+apiRouter.use('/users', usersRouter);
+
+module.exports = apiRouter;
