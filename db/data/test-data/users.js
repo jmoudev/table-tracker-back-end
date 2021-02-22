eea7c3a83d969a9d2c8cda903c19104d312eefb1
Ayako <usuajiayako@gmail.com>
2021-02-22 14:22:50 +0000

    Seed working

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,70 +1,50 @@
 module.exports = [
   {
-    email: 'harryp@gmail.com',
-    password: '234578',
     first_name: 'Harry',
     last_name: 'Potter',
     role: 'Staff'
   },
   {
-    email: 'ronw@gmail.com',
-    password: '234578',
     first_name: 'Ron',
     last_name: 'Weasley',
     role: 'Staff'
   },
   {
-    email: 'hermioneg@gmail.com',
-    password: '234578',
     first_name: 'Hermione',
     last_name: 'Grainger',
     role: 'Staff'
   },
   {
-    email: 'albusd@gmail.com',
-    password: '234578',
     first_name: 'Albus',
     last_name: 'Dumbledore',
     role: 'Staff'
   },
   {
-    email: 'joem@gmail.com',
-    password: '234578',
     first_name: 'Joe',
     last_name: 'Mould',
     role: 'Staff'
   },
   {
-    email: 'zakp@gmail.com',
-    password: '234578',
     first_name: 'Zak',
     last_name: 'Patel',
     role: 'Admin'
   },
   {
-    email: 'mela@gmail.com',
-    password: '234578',
     first_name: 'Melissa',
     last_name: 'Astbury',
     role: 'Admin'
   },
   {
-    email: 'mela@gmail.com',
-    password: '234578',
     first_name: 'Melissa',
     last_name: 'Astbury',
     role: 'Admin'
   },
   {
-    email: 'mela@gmail.com',
-    password: '234578',
-    first_name: 'Melissa',
-    last_name: 'Astbury',
+    first_name: 'Ayako',
+    last_name: 'Bland',
     role: 'Admin'
   },
   {
-    email: 'lchap@gmail.com',
-    password: '234578',
     first_name: 'Lew',
     last_name: 'Chapman',
     role: 'Admin'
