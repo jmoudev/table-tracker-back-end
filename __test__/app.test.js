252f1f74b646b9676c6216c12191dca7b21fa5ca
Zakariyya <zakp100@gmail.com>
2021-02-23 10:43:04 +0000

    return 200 for GET/api/users path

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -9,7 +9,7 @@ const app = require('../app');
 describe('/api', () => {
   afterAll(() => connection.destroy());
   beforeEach(() => connection.seed.run());
-// Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER 
+  // Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/foodItems', () => {
     it('GET 200 - responds with an array of all users', () => {
       return request(app).get('/api/food-items').expect(200);
@@ -20,5 +20,9 @@ describe('/api', () => {
   //Joe BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/tables', () => {});
   // Zak BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
-  describe('/users', () => {});
+  describe('/users', () => {
+    it('GET 200 - responds with an array of all users', () => {
+      return request(app).get('/api/users').expect(200);
+    });
+  });
 });
