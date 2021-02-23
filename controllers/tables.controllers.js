fa993b60cb49544165323994184ee2127bdcba7c
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-23 16:25:00 +0000

    completes final test for 400 error on is_active query get tables

--- controllers/tables.controllers.js
+++ controllers/tables.controllers.js
@@ -3,7 +3,9 @@ const { fetchAllTables } = require('../models/tables.models');
 exports.getAllTables = (req, res, next) => {
   const { is_active } = req.query;
 
-  fetchAllTables(is_active).then(tables => {
-    res.status(200).send({ tables });
-  });
+  fetchAllTables(is_active)
+    .then(tables => {
+      res.status(200).send({ tables });
+    })
+    .catch(next);
 };
