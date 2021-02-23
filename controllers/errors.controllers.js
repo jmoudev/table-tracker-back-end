170c4a56f3c70d8aab178cc6135cea91550f2717
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-23 16:06:08 +0000

    creates custom error handling file

---
+++ controllers/errors.controllers.js
@@ -0,0 +1,26 @@
+exports.handleRouteNotFound = (req, res, next) => {
+  next({ status: 404, msg: `Not Found` });
+};
+
+exports.handleBadRequest = (req, res, next) => {
+  next({ status: 400, msg: 'Bad Request' });
+};
+
+exports.handleMethodNotAllowed = (req, res, next) => {
+  next({ status: 405, msg: 'Method Not Allowed' });
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
+exports.handlePSQLErrors = (err, req, res, next) => {
+  if (false) {
+  } else next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  res.status(500).send({ msg: 'Internal Server Error' });
+};
