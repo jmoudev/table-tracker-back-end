ff43ada3f6e2f2c092233b1874a978a9310eaef8
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-23 11:13:48 +0000

    creates additional rough tests for patch orders as well as post and get all

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -21,29 +21,33 @@ describe('/api', () => {
     it('ERROR - status 405 - method not allowed', () => {});
 
     describe('GET all orders', () => {
-      // sucess returns all order
       xit('SUCCESS - status 200 - returns a new order', () => {});
     });
   });
 
   describe('/api/tables', () => {
     describe('/api/tables/:table_id/orders', () => {
+      describe('PATCH order by table_id', () => {
+        xit('SUCCESS - status 200 - return specified order with updated food-items', () => {});
+        xit('SUCCESS - status 200 - no information in request body does not update order', () => {});
+        xit('ERROR - status 404 - table does not exist', () => {});
+        xit('ERROR - status 404 - bad request on table_id', () => {});
+        xit('ERROR - status 404 - bad request body incorrect type', () => {});
+      });
+
       describe('POST order by table_id', () => {
-        // sucess returns new order
         xit('SUCCESS - status 201 - returns a new order', () => {});
-        // error table does not exist
-        xit('ERROR - status 404 - ', () => {});
-        // error one or more food items do not exist
-        xit('', () => {});
-        // bad request body missing required field x
-        xit('', () => {});
-        // bad request body missing multiple required fields
-        xit('', () => {});
-        // bad request?
-        xit('', () => {});
+        xit('ERROR - status 404 - table does not exist', () => {});
+        xit('ERROR - status 400 - bad request on table_id', () => {});
+        xit('ERROR - status 400 - bad request body missing required field', () => {});
+        xit('ERROR - status 400 - bad request body missing multiple required fields', () => {});
+        xit('ERROR - status 400 - bad request food-item not valid', () => {});
       });
     });
   });
 
   describe('/api/users', () => {});
 });
+
+// get tables (in order of first in first served)
+// post tables - (extra)
