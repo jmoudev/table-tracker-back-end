a3e102d8006fe04db180bc39db64379cdc62fe0a
Zakariyya <zakp100@gmail.com>
2021-02-23 16:59:49 +0000

    add error handling middleware

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const {
+  handleRouteNotFound,
+  handleCustomErrors,
+  handlePSQLErrors,
+  handleServerErrors
+} = require('./controllers/errors.controllers');
 const app = express();
 const apiRouter = require('./routers/api.router');
 
 app.use('/api', apiRouter);
+
+app.all('/*', handleRouteNotFound);
+
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
