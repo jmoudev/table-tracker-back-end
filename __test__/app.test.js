262d91ff0588821afcc46058b2b6f30575b8a0f7
Ayako <usuajiayako@gmail.com>
2021-02-23 09:50:42 +0000

    added models files and comments on test file

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -9,13 +9,16 @@ const app = require('../app');
 describe('/api', () => {
   afterAll(() => connection.destroy());
   beforeEach(() => connection.seed.run());
-
+// Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER 
   describe('/foodItems', () => {
     it('GET 200 - responds with an array of all users', () => {
       return request(app).get('/api/food-items').expect(200);
     });
   });
+  //Joe BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/orders', () => {});
+  //Joe BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/tables', () => {});
+  // Zak BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/users', () => {});
 });
