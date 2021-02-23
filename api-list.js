2409ebe0501391bfed4f6ea04cc50fa87eb2d1a2
JJYoko101 <mould_joe@hotmail.com>
2021-02-23 11:13:48 +0000

    creates additional rough tests for patch orders as well as post and get all

--- api-list.js
+++ api-list.js
@@ -3,13 +3,13 @@ module.exports = {
     description:
       'serves up a json representation of all the available endpoints of the api'
   },
-  'POST /orders': {
+  'POST /table/:table_id/orders': {
     description:
       'creates a new order for requested table and responds with the new order object',
     queries: null,
     exampleRequestBody: {
       table_name: 'Table 3',
-      food_array: ['Beef Tacos', 'Lamb Tacos', 'etc'],
+      food_array: ['beef tacos', 'lamb tacos', 'etc'],
       description: 'Nut allergy'
     },
     exampleResponse: {
