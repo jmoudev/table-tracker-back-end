6336ec8324c741d12abd5950ee7605ed2b1f4762
JJYoko101 <mould_joe@hotmail.com>
2021-02-23 15:30:08 +0000

    corrections to test file in main

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -9,18 +9,18 @@ const app = require('../app');
 describe('/api', () => {
   afterAll(() => connection.destroy());
   beforeEach(() => connection.seed.run());
-<<<<<<< 
-// Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER 
+
+  // Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/food-items', () => {
     it('GET 200 - responds with an array of all food_items', () => {
       return request(app)
-      .get('/api/food-items')
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.foodItems).toHaveLength(21)
-      })
-    })
-  })
+        .get('/api/food-items')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.foodItems).toHaveLength(21);
+        });
+    });
+  });
   //Joe BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/api/orders', () => {
     it('ERROR - status 405 - method not allowed', () => {});
@@ -61,5 +61,4 @@ describe('/api', () => {
   });
   // Zak BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER
   describe('/users', () => {});
-
 });
