546843d11cb42d595650ab8a44fdef05116f59f9
Ayako <usuajiayako@gmail.com>
2021-02-23 16:34:19 +0000

    Add POST /api/foodItems

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -10,13 +10,24 @@ describe('/api', () => {
   afterAll(() => connection.destroy());
   beforeEach(() => connection.seed.run());
 // Ayako BRANCH OUT FOR EACH REQUEST!! DON'T WORK ON MASTER 
-  describe('/food-items', () => {
-    it('GET 200 - responds with an array of all food_items', () => {
+  describe('GET /api/food-items', () => {
+    it('SUCCESS status 200 - responds with an array of all food_items', () => {
       return request(app)
       .get('/api/food-items')
       .expect(200)
       .then(({ body }) => {
         expect(body.foodItems).toHaveLength(21)
+      })
+    })
+  })
+  describe('POST /api/food-items', () => {
+    it('SUCCESS POST 201 - adds a food item to food_items table', () => {
+      return request(app)
+      .post('/api/food-items')
+      .send({ name: 'Pina Colada', price: 8.00, course: 'drinks'})
+      .expect(201)
+      .then(({ body }) => {
+        expect(body.foodItems.name).toBe('Pina Colada')
       })
     })
   })
