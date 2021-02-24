db12b63f938a2e39462db3480cc8f570445404c2
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-24 16:02:25 +0000

    adds test data for orders / orders junction table

--- db/data/test-data/orders-food-junc.js
+++ db/data/test-data/orders-food-junc.js
@@ -1,6 +1,58 @@
 module.exports = [
   {
-    order_id: '',
-    food_item_id: ''
+    order_id: '1',
+    food_item_id: '2'
+  },
+  {
+    order_id: '1',
+    food_item_id: '1'
+  },
+  {
+    order_id: '1',
+    food_item_id: '6'
+  },
+  {
+    order_id: '2',
+    food_item_id: '2'
+  },
+  {
+    order_id: '2',
+    food_item_id: '7'
+  },
+  {
+    order_id: '2',
+    food_item_id: '8'
+  },
+  {
+    order_id: '2',
+    food_item_id: '9'
+  },
+  {
+    order_id: '2',
+    food_item_id: '10'
+  },
+  {
+    order_id: '2',
+    food_item_id: '11'
+  },
+  {
+    order_id: '2',
+    food_item_id: '12'
+  },
+  {
+    order_id: '2',
+    food_item_id: '13'
+  },
+  {
+    order_id: '2',
+    food_item_id: '14'
+  },
+  {
+    order_id: '2',
+    food_item_id: '15'
+  },
+  {
+    order_id: '2',
+    food_item_id: '16'
   }
 ];
