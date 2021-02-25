c83915fc941b29ca85691578356928838b611f1d
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-25 12:02:05 +0000

    merge changes from ayako patch food items

--- controllers/food-items.controllers.js
+++ controllers/food-items.controllers.js
@@ -1,16 +1,33 @@
-const { selectFoodItems, appendFoodItems } = require("../models/food-items.models")
+const {
+  selectFoodItems,
+  appendFoodItems,
+  amendFoodItemsById
+} = require('../models/food-items.models');
 
 //GET
 exports.getFoodItems = (req, res, next) => {
-  selectFoodItems().then((foodItems) => {
-    res.send({ foodItems })
-  })
-}
+  selectFoodItems().then(foodItems => {
+    res.send({ foodItems });
+  });
+};
 
 //POST
 exports.addFoodItems = (req, res, next) => {
   const { name, price, course } = req.body;
-  appendFoodItems(name, price, course).then((foodItems) => {
-    res.status(201).send(foodItems)
-  }).catch(next)
-}
+  appendFoodItems(name, price, course)
+    .then(foodItems => {
+      res.status(201).send(foodItems);
+    })
+    .catch(next);
+};
+
+//PATCH
+exports.updateFoodItemsById = (req, res, next) => {
+  const { food_item_id } = req.params;
+  const { name, price, course } = req.body;
+  amendFoodItemsById(food_item_id, name, price, course)
+    .then(foodItems => {
+      res.status(201).send(foodItems);
+    })
+    .catch(next);
+};
--- routers/food-items.router.js
+++ routers/food-items.router.js
@@ -1,6 +1,11 @@
 const foodItemsRouter = require('express').Router();
-const { getFoodItems, addFoodItems } = require('../controllers/food-items.controllers')
+const {
+  getFoodItems,
+  addFoodItems,
+  updateFoodItemsById
+} = require('../controllers/food-items.controllers');
 
 foodItemsRouter.route('/').get(getFoodItems).post(addFoodItems);
+foodItemsRouter.route('/:food_item_id').patch(updateFoodItemsById);
 
 module.exports = foodItemsRouter;
