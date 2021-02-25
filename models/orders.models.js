5c2409c840bc25bed8f25aa4fc0898f901c2d258
JJYoko101 <mould_joe@hotmail.com>
2021-02-25 08:58:53 +0000

    posts food items to order junc table returning correct return order object

--- models/orders.models.js
+++ models/orders.models.js
@@ -2,17 +2,24 @@ const connection = require('../db/connection');
 
 exports.sendOrderByTableId = (table_id, order) => {
   const { description, food_items } = order;
-  // post order first
-  // in return from post order get order_id
-  // post into junction table with order_id
 
   return connection('orders')
     .insert({ table_id, description })
     .returning('*')
     .then(([order]) => {
       const { order_id } = order;
-      console.log(food_items);
 
-      // return connection('orders_food_junc').insert();
+      const junc_pairs = food_items.map(food_item_id => {
+        return { order_id, food_item_id };
+      });
+
+      return connection('orders_food_junc')
+        .insert(junc_pairs)
+        .then(() => {
+          const orderWithFoodItems = { ...order };
+          orderWithFoodItems.food_items = food_items;
+
+          return orderWithFoodItems;
+        });
     });
 };
