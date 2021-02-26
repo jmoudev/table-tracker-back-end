d30d65abbe13c75fe7d639ee4b53643796f6a67d
Ayako <usuajiayako@gmail.com>
2021-02-26 16:21:05 +0000

    Adds development-data

--- db/data/development-data/food-items.js
+++ db/data/development-data/food-items.js
@@ -1,7 +1,107 @@
 module.exports = [
   {
-    name: '',
-    price: '',
-    course: ''
+    name: 'Garlic Bread',
+    price: 3.00,
+    course: 'starter'
+  },
+  {
+    name: 'Prawn Skewers',
+    price: 4.50,
+    course: 'starter'
+  },
+  {
+    name: 'Nachos and Cheese',
+    price: 4.50,
+    course: 'starter'
+  },
+  {
+    name: 'Potato Skins',
+    price: 3.50,
+    course: 'starter'
+  },
+  {
+    name: 'Cheesy Garlic Bread',
+    price: 3.50,
+    course: 'starter'
+  },
+  {
+    name: 'Beef Taco',
+    price: 10.50,
+    course: 'main'
+  },
+  {
+    name: 'Lamb Taco',
+    price: 11.00,
+    course: 'main'
+  },
+  {
+    name: 'Lasagne',
+    price: 7.00,
+    course: 'main'
+  },
+  {
+    name: 'Seafood Platter',
+    price: 15.00,
+    course: 'main'
+  },
+  {
+    name: 'Seafood Platter Deluxe',
+    price: 24.50,
+    course: 'main'
+  },
+  {
+    name: 'Orange Juice',
+    price: 2.90,
+    course: 'drinks'
+  },
+  {
+    name: 'Tequila Shot',
+    price: 4.00,
+    course: 'drinks'
+  },
+  {
+    name: 'Margharita',
+    price: 7.50,
+    course: 'drinks'
+  },
+  {
+    name: 'Fanta',
+    price: 2.70,
+    course: 'drinks'
+  },
+  {
+    name: 'Coca Cola',
+    price: 2.95,
+    course: 'drinks'
+  },
+  {
+    name: 'Water',
+    price: 1.80,
+    course: 'drinks'
+  },
+  {
+    name: 'Rice Pudding',
+    price: 3.00,
+    course: 'dessert'
+  },
+  {
+    name: 'Cheesecake',
+    price: 4.00,
+    course: 'dessert'
+  },
+  {
+    name: 'Pineapple Upside Down Cake',
+    price: 3.50,
+    course: 'dessert'
+  },
+  {
+    name: 'Waffles and Ice Cream',
+    price: 4.50,
+    course: 'dessert'
+  },
+  {
+    name: 'Sticky Toffee Pudding',
+    price: 5.50,
+    course: 'dessert'
   }
 ];
