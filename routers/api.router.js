417da0c9bc4ead42d1b8c2af9694e706a327ec6f
Ayako <usuajiayako@gmail.com>
2021-02-26 09:43:22 +0000

    adds catch block

--- routers/api.router.js
+++ routers/api.router.js
@@ -5,7 +5,7 @@ const usersRouter = require('./users.router');
 
 const apiRouter = require('express').Router();
 
-apiRouter.use('/food-items', foodItemsRouter);
+apiRouter.use('/food_items', foodItemsRouter);
 apiRouter.use('/orders', ordersRouter);
 apiRouter.use('/tables', tablesRouter);
 apiRouter.use('/users', usersRouter);
