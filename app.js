bedca4e47cc1783ca32fe76ededb9df47d72db46
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-26 15:01:59 +0000

    Added cors so works on FE

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const {
   handleRouteNotFound,
   handleCustomErrors,
   handlePSQLErrors,
-  handleServerErrors
+  handleServerErrors,
 } = require('./controllers/errors.controllers');
 const app = express();
 const apiRouter = require('./routers/api.router');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
