01845babf80cfaa519b573fd8143dde7f478fc87
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2021-02-26 11:11:11 +0000

    Update food-items.controllers.js
    Back to catch(next)

--- controllers/food-items.controllers.js
+++ controllers/food-items.controllers.js
@@ -8,7 +8,7 @@ const {
 exports.getFoodItems = (req, res, next) => {
   selectFoodItems().then(foodItems => {
     res.send({ foodItems });
-  }).catch((err) => {console.log(err)})
+  }).catch(next)
 };
 
 //POST
