f2add1af0c7069ac6a82649cc9690207c7c7903a
Melissa Astbury <melissaastbury@hotmail.com>
2021-03-03 08:42:52 +0000

    added boiler plate code to link up socket.io on the listen.js file

--- listen.js
+++ listen.js
@@ -1,7 +1,15 @@
 const app = require('./app');
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 
 const PORT = 9090;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+// io.on('connection', (socket) => {
+//   socket.on('new-order', (order) => {
+//     io.emit('order', order);
+//   });
+// });
